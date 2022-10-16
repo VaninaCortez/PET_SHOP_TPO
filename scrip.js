7839e21f2e57a53d04997d18f1ecd820abd959a8
VaninaCortez <vany_vc@hotmail.com>
2022-10-16 16:27:00 -0300

    formulario corina

---
+++ scrip.js
@@ -0,0 +1,6 @@
+let question = document.querySelectorAll(’.question’);
+let btnDropdown = document.querySelectorAll(’.questio .more’);
+let answer = document.querySelectorAll(’.answer’);
+let parrafo = document.querySelectorAll(’.answer p’);
+
+for ( let )
